13a7884da52e9513cb6a4890adf25a40e6d39418
Ryan <RR.Schweitzer@gmail.com>
2015-03-27 16:05:52 -0600

    finished

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ app.get('/', function(req, res) {
 Passport.use(new GoogleStrategy({
 	clientID: process.env.GOOGLE_CLIENTID,
 	clientSecret: process.env.GOOGLE_SECRET,
-	callbackURL: 'http://recycleut.org:' + port + '/auth/google/callback'
+	callbackURL: 'http://www.recycleut.org' + '/auth/google/callback'
 }, function(token, tokenSecret, profile, done) {
 			userCtrl.updateOrCreate(profile)
 				.then(function(user) {
